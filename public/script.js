94be2fe8bd0be8004b226810ba8464d6c5339c1c
Nikola Ristic <nikola@ogitive.com>
2018-05-13 17:30:49 +0200

    ...

---
+++ public/script.js
@@ -0,0 +1,67 @@
+var PRICE = 9.99;
+new Vue({
+    el: '#app',
+    data: {
+        total: 0,
+        items: [],
+        cart: [],
+        newSearch: '',
+        lastSearch: '',
+        loading: false
+    },
+    methods: {
+        onSubmit: function () {
+            this.items = [];
+            this.loading = true;
+            this.$http
+                .get('/search/'.concat(this.newSearch))
+                .then(function (res) {
+                    this.lastSearch = this.newSearch;
+                    this.items = res.data;
+                    this.loading = false;
+                });
+
+        },
+        addItem: function (index) {
+            this.total += PRICE;
+            var item = this.items[index];
+            var found = false;
+            for (var i = 0; i < this.cart.length; i++) {
+                if (this.cart[i].id === item.id) {
+                    found = true;
+                    this.cart[i].qty++;
+                    break;
+                }
+            }
+            if (!found) {
+                this.cart.push({
+                    id: item.id,
+                    title: item.title,
+                    qty: 1,
+                    price: PRICE
+                });
+            }
+        },
+        inc: function (item) {
+            item.qty++;
+            this.total += PRICE;
+        },
+        dec: function (item) {
+            item.qty--;
+            this.total -= PRICE;
+            if (item.qty <= 0) {
+                for (var i = 0; i < this.cart.length; i++) {
+                    if (this.cart[i].id === item.id) {
+                        this.cart.splice(i, 1);
+                        break;
+                    }
+                }
+            }
+        }
+    },
+    filters: {
+        formatPrice: function (price) {
+            return '$'.concat(price.toFixed(2));
+        }
+    }
+});
